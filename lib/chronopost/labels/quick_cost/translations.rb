6bdf4b5d96453002f5c1b03879990596580af420
Žilvinas Verseckas <zverseckas@gmail.com>
2019-01-09 17:29:23 +0200

    Remove redundant translations

--- lib/chronopost/labels/quick_cost/translations.rb
+++ lib/chronopost/labels/quick_cost/translations.rb
@@ -6,12 +6,9 @@ module Chronopost
       module Translations
         PARAMS = {
           account_number: :accountNumber,
-          password: :password,
           departure_code: :depCode,
           arrival_code: :arrCode,
-          weight: :weight,
           product_code: :productCode,
-          type: :type,
         }.freeze
 
         RESPONSE = {}.freeze
